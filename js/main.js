7b1ca89df27109162fca97eb514f6654148c7c16
JMK <ds420114@naver.com>
2023-10-04 02:20:08 +0900

    Editting section1,2 & banner

--- js/main.js
+++ js/main.js
@@ -26,6 +26,81 @@ $(function(){
         }
     });
 
-    
+    // 배너
+    let bnnNum = 0;
 
+        // next button
+    $(".next").on("click",function(){
+        bnnNum ++;
+        if (bnnNum > 4) bnnNum = 0
+        book_w = $("#banner").width();
+        $("#banner ul").animate({left:-bnnNum*book_w},500,function(){
+            $(".idc img").attr("src","img/state_out.png");
+            $(".idc img").eq(bnnNum).attr("src","img/state_over.png");
+        })
+    });
+        // prev button
+    $(".prev").on("click",function(){
+        bnnNum--;
+        if(bnnNum < 0) bnnNum = 4
+        book_w = $("#banner").width();
+        $("#banner ul").animate({left:-bnnNum*book_w},500,function(){
+            $(".idc img").attr("src","img/state_out.png");
+            $(".idc img").eq(bnnNum).attr("src","img/state_over.png");
+        })
+    });
+        // indicator
+    $(".idc li a").on("click", function(){
+        let strName = $(this).parent().attr("id");
+        slideTarget(strName.substr(3,1));
+        return false;
+    })
+    function slideTarget(n) {
+        const pos = Number(n)*-100+"%";
+        $("#banner ul").animate({left:pos},500,function(){
+            $(".idc img").attr("src","img/state_out.png");
+            $(".idc img").eq(n).attr("src","img/state_over.png");
+        })
+    }
+        // 자동 슬라이드
+    let btnNum=0;
+    setInterval(function(){
+        btnNum++;
+        if(btnNum>4) btnNum=0;
+        $(".idc li a").eq(btnNum).trigger("click");
+        //trigger : 강제로 이벤트 실행시킴
+    },4000);
+
+        // 이벤트 패널 인디케이터
+        $("#section1_idc li a").on("click", function(){
+            let strIDC = $(this).parent().attr("id");
+            slideEVT(strIDC.substr(8,1));
+            return false;
+        })
+        function slideEVT(n) {
+            const pos = Number(n)*-100+"%";
+            $("#pageAll").animate({left:pos},500,function(){
+                $("#section1_idc img").attr("src","img/state_out_gray.png");
+                $("#section1_idc img").eq(n).attr("src","img/state_over.png");
+            })
+        }
+        let evtNum = 0;
+        $(".sec1_next").on("click", function(){
+            evtNum ++;
+            if (evtNum > 1) evtNum = 0
+            $("#pageAll").animate({left:-evtNum*100+"%"},500,function(){
+                $("#section1_idc img").attr("src","img/state_out_gray.png");
+                $("#section1_idc img").eq(evtNum).attr("src","img/state_over.png");
+            });
+            return false;
+        });
+        $(".sec1_prev").on("click", function(){
+            evtNum --;
+            if (evtNum < 0) evtNum = 1
+            $("#pageAll").animate({left:-evtNum*100+"%"},500,function(){
+                $("#section1_idc img").attr("src","img/state_out_gray.png");
+                $("#section1_idc img").eq(evtNum).attr("src","img/state_over.png");
+            });
+            return false;
+        });
 });
