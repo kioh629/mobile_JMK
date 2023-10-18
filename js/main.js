c94c0b8fe2c0594480cf73940dbae5dbec4a78f5
kioh629 <ds420114@naver.com>
2023-10-18 14:57:50 +0900

    section1,2 slick 적용

--- js/main.js
+++ js/main.js
@@ -38,42 +38,31 @@ $(function(){
         slidesToScroll: 1,  // 한번에 슬라이드 시킬 아이템 수
     });
 
+    // section1 - slick 적용
+    $("#pageAll").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+    })
 
-        // 이벤트 패널 인디케이터
-        $("#section1_idc li a").on("click", function(){
-            let strIDC = $(this).parent().attr("id");
-            slideEVT(strIDC.substr(8,1));
-            return false;
-        })
-        function slideEVT(n) {
-            const pos = Number(n)*-100+"%";
-            $("#pageAll").animate({left:pos},300,function(){
-                $("#section1_idc img").attr("src","img/state_out_gray.png");
-                $("#section1_idc img").eq(n).attr("src","img/state_over.png");
-            })
-        }
-        let evtNum = 0;
-        $(".sec1_next").on("click", function(){
-            evtNum ++;
-            if (evtNum > 1) evtNum = 0
-            $("#pageAll").animate({left:-evtNum*100+"%"},300,function(){
-                $("#section1_idc img").attr("src","img/state_out_gray.png");
-                $("#section1_idc img").eq(evtNum).attr("src","img/state_over.png");
-            });
-            return false;
-        });
-        $(".sec1_prev").on("click", function(){
-            evtNum --;
-            if (evtNum < 0) evtNum = 1
-            $("#pageAll").animate({left:-evtNum*100+"%"},300,function(){
-                $("#section1_idc img").attr("src","img/state_out_gray.png");
-                $("#section1_idc img").eq(evtNum).attr("src","img/state_over.png");
-            });
-            return false;
-        });
+    // section2 -slick 적용
+    $("#ch_thum").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true
+    });
+    // section2 -
+    $("#ch_thum ul a").on("click",function(){
+        const video_src = $(this).find("img").attr("data-src");
+        $("#ch_video li iframe").attr("src",video_src);
+        return false;
+    });
+
+
+
     // header 색 변화
     $(window).on("scroll",function(){
-        if($(window).scrollTop()> 0){
+        if($(window).scrollTop() != 0){
             $("#header").css("background-color","rgba(255,226,0,1)");
             $(".search img").attr("src","img/search.png");
             $("#logo h1 a img").attr("src","img/netmarble.png");
